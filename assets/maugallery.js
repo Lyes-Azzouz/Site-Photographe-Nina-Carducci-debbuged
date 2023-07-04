17df970562bf3bbc74afac40736e5bbb5bba81fd
ZyesSubMod <azzouz.lyes.w@gmail.com>
2023-07-04 17:49:16 +0200

    correction width modal-content

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -179,7 +179,7 @@
         lightboxId ? lightboxId : "galleryLightbox"
       }" tabindex="-1" role="dialog" aria-hidden="true">
           <div class="modal-dialog" role="document">
-            <div class="modal-content">
+            <div class="modal-content " style = "width : 400px">
               <div class="modal-body">
                 ${
                   navigation
